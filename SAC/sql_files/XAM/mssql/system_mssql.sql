41df6556e9caf7f68c4b066bd50a3508d2208c83
Pavan Ofc Lap <simhadri.pavans@gmail.com>
2018-06-28 11:44:49 -0400

    initial sync from local to git

--- SAC/sql_files/XAM/mssql/system_mssql.sql
+++ SAC/sql_files/XAM/mssql/system_mssql.sql
@@ -47,7 +47,7 @@ GO
 
 
 -- This is used internally.
-INSERT INTO "system_database_schema_version" VALUES(10, 56);
+INSERT INTO "system_database_schema_version" VALUES(10, 57);
 GO
 
 -- TABLE "system_data"
