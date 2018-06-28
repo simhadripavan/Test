146035e49d2c0ebfd35cd72aba2581fa30d18cd7
Pavan Ofc Lap <simhadri.pavans@gmail.com>
2018-06-28 13:45:43 -0400

    Schema versioning related update for systemDB.Automated submit by build script.

--- SAC/sql_files/xam/mssql/system_mssql.sql
+++ SAC/sql_files/xam/mssql/system_mssql.sql
@@ -47,7 +47,7 @@ GO
 
 
 -- This is used internally.
-INSERT INTO "system_database_schema_version" VALUES(10, 58);
+INSERT INTO "system_database_schema_version" VALUES(10, 59);
 GO
 
 -- TABLE "system_data"
