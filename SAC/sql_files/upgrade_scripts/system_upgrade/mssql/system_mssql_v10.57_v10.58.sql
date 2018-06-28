bac4186306bd6f9aa676706aba8cc3ae87c56879
Pavan Ofc Lap <simhadri.pavans@gmail.com>
2018-06-28 11:59:30 -0400

    Schema versioning related update for systemDB.Automated submit by build script.

---
+++ SAC/sql_files/upgrade_scripts/system_upgrade/mssql/system_mssql_v10.57_v10.58.sql
@@ -0,0 +1,3 @@
+-- Updating the database schema version
+UPDATE system_database_schema_version
+SET minor=58;
--- SAC/sql_files/xam/postgres/system_postgres.sql
+++ SAC/sql_files/xam/postgres/system_postgres.sql
@@ -47,7 +47,7 @@ CREATE TABLE system_database_schema_version (
 );
 
 -- This is used internally.
-INSERT INTO system_database_schema_version VALUES(10, 57);
+INSERT INTO system_database_schema_version VALUES(10, 58);
 
 -- TABLE "system_data"
 -- Stores system wide data common to all tenants
