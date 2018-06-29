4cae88a3e5be9e21843bdc3087f7e3e2b4d29e20
Pavan Simhadri <pavan.simhadri@citrix.com>
2018-06-29 10:25:24 -0400

    Schema versioning related update.Automated submit by build script.

--- SAC/sql_files/xam/mssql/mam_mssql.sql
+++ SAC/sql_files/xam/mssql/mam_mssql.sql
@@ -483,7 +483,7 @@ CREATE TABLE system_status (
 GO
 
 -- This is used internally.
-INSERT INTO database_schema_version VALUES(10, 62);
+INSERT INTO database_schema_version VALUES(10, 63);
 GO
 
 -- Stocktake that stored device info and installed apps on the device
