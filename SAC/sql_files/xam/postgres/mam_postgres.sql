3e8495d96ec87678f0029564903b4004633e30bd
Pavan Simhadri <pavan.simhadri@citrix.com>
2018-07-02 13:00:44 -0400

    Schema versioning related update for tenant DB files.Automated submit by build script.

--- SAC/sql_files/xam/postgres/mam_postgres.sql
+++ SAC/sql_files/xam/postgres/mam_postgres.sql
@@ -478,7 +478,7 @@ CREATE TABLE system_status (
 );
 
 -- This is used internally.
-INSERT INTO database_schema_version VALUES(10, 60);
+INSERT INTO database_schema_version VALUES(10, 61);
 
 ---Stocktake that stored device info and installed apps on the device
 CREATE TABLE store_stocktake
