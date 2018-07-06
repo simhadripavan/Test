bef9f27f4dbd2c5318523c72bb92b7bbe5131e9a
MPGCI Support <mpgci@citrix.com>
2018-07-06 10:53:21 -0400

    Schema versioning related update for system DB files.	Automated submit by build script.

--- SAC/sql_files/xam/mssql/system_mssql.sql
+++ SAC/sql_files/xam/mssql/system_mssql.sql
@@ -47,7 +47,7 @@ GO
 
 
 -- This is used internally.
-INSERT INTO "system_database_schema_version" VALUES(10, 70);
+INSERT INTO "system_database_schema_version" VALUES(10, 71);
 GO
 
 -- TABLE "system_data"
