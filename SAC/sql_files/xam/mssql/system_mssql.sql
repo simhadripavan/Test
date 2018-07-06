abdd1bc5300c8c70776961e89ccc1630249d5031
Pavan Simhadri <pavan.simhadri@citrix.com>
2018-07-06 10:40:07 -0400

    Schema versioning related update for system DB files.	Automated submit by build script.

--- SAC/sql_files/xam/mssql/system_mssql.sql
+++ SAC/sql_files/xam/mssql/system_mssql.sql
@@ -47,7 +47,7 @@ GO
 
 
 -- This is used internally.
-INSERT INTO "system_database_schema_version" VALUES(10, 68);
+INSERT INTO "system_database_schema_version" VALUES(10, 69);
 GO
 
 -- TABLE "system_data"
